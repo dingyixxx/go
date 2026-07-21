c95b7cc1986ff92cdbe2b0f9629926d635bd597a
dingyi <dingyixxx@126.com>
2026-07-21 23:06:01 +0800

    dos

--- 02.return2Results.go
+++ 02.return2Results.go
@@ -31,3 +31,27 @@ func main() {
 //--project02
 //-----main
 //-----package
+
+//dos
+//md test001 mkdir
+//dir pwd
+//切换盘符 c:
+
+//md ok001 ok002 ok003
+//rd ok001
+//删掉非空目录会报错 rd ok002      目录不是空的。
+//rd /q/s ok002 rm -rf
+//C:\Users\P72\Desktop\go>rd /s ok003
+//ok003, 是否确认(Y/N)?
+
+//echo i love you my baby daughter > ./abc.txt
+//copy abc.txt  abc1.txt
+//copy abc.txt  C:\Users\P72\Desktop\go\ok002
+//move abc.txt ./ok003/
+//del *
+//del xxx
+
+//cls
+//exit
+//echo.>news.txt
+//move news.txt ../b/
