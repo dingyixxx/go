f01848e8cf87521f6e9d9b368ea0913ae0828de7
Your Name <dingyixxx@126.com>
2026-07-25 22:55:17 +0800

    strconv.format && sprintf

--- goproject/src/go_code/transtype05/main.go
+++ goproject/src/go_code/transtype05/main.go
@@ -12,10 +12,17 @@ func main() {
 
 	//	第二种方式
 	var num3 = 99
-	//var num4 float64=23.456
-	//var b2 bool=true
+	var num4 float64 = 23.456
+	var b2 bool = true
 
 	str1 := strconv.FormatInt(int64(num3), 10)
 	fmt.Printf("%T,%q \n", str1, str1)
+
+	str2 := strconv.FormatFloat(num4, 'f', 10, 64)
+	//说明：'f'格式 10:小数点位保留10位 64：表示这个小数是float64
+	fmt.Printf("%T,%q \n", str2, str2)
+
+	str3 := strconv.FormatBool(b2)
+	fmt.Printf("str3:%T,%q", str3, str3)
 
 }
